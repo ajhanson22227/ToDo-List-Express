653a5d857a2f6addee2b71b27b0d96913a8fb175
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-09 15:00:21 -0600

    Add login post method, returns token and username fix readme typo

--- controllers/User.js
+++ controllers/User.js
@@ -26,3 +26,24 @@ exports.create = async (req, res) => {
     res.status(400).json(err);
   }
 };
+
+exports.login = async (req, res) => {
+  //check for username in db
+  const user = await User.findOne({ username: req.body.username });
+  if (!user) {
+    return res.status(400).json({ loginError: "Invalid username" });
+  }
+
+  //check if password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) {
+    return res.status(400).json({ passwordError: "Invalid Password" });
+  }
+
+  //create and sign user token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.json({
+    username: user.username,
+    token: token,
+  });
+};
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/User");
 
 /* GET users listing. */
 router.post("/create", userController.create);
+router.post("/login", userController.login);
 
 module.exports = router;
