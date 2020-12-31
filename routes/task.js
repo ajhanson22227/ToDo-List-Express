c3f9a94482dcb5064b9aa014bb7c8c4d4c26cb74
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-31 11:34:47 -0600

    Add route for task delete

--- routes/task.js
+++ routes/task.js
@@ -5,5 +5,6 @@ const taskController = require('../controllers/Task');
 
 /* GET users listing. */
 router.post('/:projid/create', taskController.create);
+router.post('/:taskid/delete', taskController.delete);
 
 module.exports = router;
