a2910f4dbb7acbdb4d036ca726160aa2579bf189
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-04 16:48:47 -0600

    wildcard

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(function (req, res, next) {
 });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   // res.header('Access-Control-Allow-Headers', 'Content-Type, user-token');
