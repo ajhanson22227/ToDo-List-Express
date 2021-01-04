ab4825bfc2c1dc98a84c60843f89006d2e9c8d50
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-04 15:01:12 -0600

    Add update route

--- routes/task.js
+++ routes/task.js
@@ -6,5 +6,6 @@ const taskController = require('../controllers/Task');
 /* GET users listing. */
 router.post('/:projid/create', taskController.create);
 router.post('/:taskid/delete', taskController.delete);
+router.post('/:taskid/update', taskController.update);
 
 module.exports = router;
